758f4c1f7b68a12f443f4606e3355ee7369d3178
sagar19197 <sagar19197@iiitd.ac.in>
2023-02-19 17:18:21 +0530

    gRPC: Completed proto file for Server

--- gRPC/client.py
+++ gRPC/client.py
@@ -12,15 +12,41 @@ import uuid;
 client_id = str(uuid.uuid1());
 print("\nWELCOME CLIENT USER, YOUR UUID:",client_id);
 
-print("Fetching List of active Servers from Registry SERVER...\n");
-channel = grpc.insecure_channel("localhost:8000");
 
-# ClientStub - 
-client_stub =  RegistryServer_pb2_grpc.GetServerListServiceStub(channel);
-registry_server_request = RegistryServer_pb2.ClientRequest(client_id = client_id);
-registry_server_response = client_stub.GetServerList(registry_server_request);
 
-serverList = registry_server_response.serverList.servers;
-# Printing  information abot servers -
-for server in serverList:
-	print(server.name,"-",server.address);
+def GetServerList():
+
+	print("Fetching List of active Servers from Registry SERVER...\n");
+	channel = grpc.insecure_channel("localhost:8000");
+	# ClientStub - 
+	client_stub =  RegistryServer_pb2_grpc.GetServerListServiceStub(channel);
+	registry_server_request = RegistryServer_pb2.ClientRequest(client_id = client_id);
+	registry_server_response = client_stub.GetServerList(registry_server_request);
+
+	serverList = registry_server_response.serverList.servers;
+	# Printing  information abot servers -
+	for server in serverList:
+		print(server.name,"-",server.address);
+
+
+
+
+while(True):
+	print("-------------------------------------");
+	print("Please Enter Number of one of the following options:");
+	print("1. GetServerList");
+	print("2. JoinServer");
+	print("3. LeaveServer");
+	print("4. GetArticles");
+	print("5. PublishArticles");
+	print("6. EXIT");
+
+	option = input("\n Enter Option Number : ");
+	print("----------------------------------------")
+	# Case handling - 
+	if option == "1":
+		GetServerList();
+	elif option == "6":
+		break;
+	else:
+		print("Invalid Input!! Please Enter again.");
