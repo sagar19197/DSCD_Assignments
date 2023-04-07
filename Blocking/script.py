638b915b92c935eb4f51a633ca638fda4b806bad
Sagar19197 <sagar19197@iiitd.ac.in>
2023-04-07 12:58:51 +0530

    Adding time.sleep() solved the unary_unary bug

--- Blocking/script.py
+++ Blocking/script.py
@@ -2,11 +2,12 @@
 # Script File For Assignment-2
 
 import subprocess
+import time
+
 
 # for N replicas
 N = int(input("\nEnter Number of Replicas/Servers : "));
 
-# Starting Registry Server
 commands = ['python registry_server.py'];
 
 # Starting N replicas from 5000 ports
@@ -15,12 +16,15 @@ for i in range(N):
     address = "localhost:" + str(port);
     commands.append('python server.py '+address);
 
+
 # Get the path of the cmd.exe executable
 cmd_path = subprocess.check_output('where cmd.exe').decode().split()[0]
 
 # Open a new command prompt window for each Server and Registry Server
 for command in commands:
     process = subprocess.Popen([cmd_path, '/c', 'start', 'cmd', '/k', command]);
+    time.sleep(0.1);
+
 
 
 # for M Clients -
@@ -29,6 +33,7 @@ M = int(input("\nEnter Number of Clients to generate : "));
 commands = [];
 for i in range(M):
     commands.append('python client.py');
+    time.sleep(0.1);
 
 # Open a new command prompt window for each Client
 for command in commands:
