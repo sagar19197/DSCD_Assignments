90fa86a0e91b8403022d7cd31d9a0f5a101596ba
Sagar19197 <sagar19197@iiitd.ac.in>
2023-04-14 10:47:06 +0530

    QUORAM-BASED: Write Operation Completed

--- Quoram-Based/client.py
+++ Quoram-Based/client.py
@@ -7,6 +7,10 @@ import grpc
 import RegistryServer_pb2;
 import RegistryServer_pb2_grpc;
 
+# Importing Server Proto - 
+import Server_pb2;
+import Server_pb2_grpc;
+
 import uuid;
 
 #----------------------------------------------------
@@ -34,3 +38,108 @@ for server in serverList:
 
 
 #---------------------------------------------------------
+
+def generateUID():
+	return str(uuid.uuid1());
+
+
+
+def getServerList():
+	getServerList_operation("all");
+
+
+
+def getServerList_operation(requestType):
+	global CHANNEL;
+
+	# ClientStub - 
+	CLIENT_STUB =  RegistryServer_pb2_grpc.GetServerListServiceStub(CHANNEL);
+	registry_server_request = RegistryServer_pb2.ClientRequest(client_id = client_id, requestType = requestType);
+	registry_server_response = CLIENT_STUB.GetServerList(registry_server_request);
+
+	new_serverList = registry_server_response.serverList.servers;
+	# Printing  information abot servers -
+	print("Following Information is Recieved from REGISTRY SERVER:");
+	for server in new_serverList:
+		print(server.address);
+
+	return new_serverList;
+
+
+
+
+def Write(filename, content, file_uuid):
+	print("\nPROCESSING YOUR REQUEST \n");
+
+	# Contacting Registry Servers for Fetching list of N_w Servers
+	new_serverList = getServerList_operation("write");
+
+	for server in new_serverList:
+		# Generating channel
+		channel = grpc.insecure_channel(server.address);
+		server_stub = Server_pb2_grpc.ClientWriteServiceStub(channel);
+		clientWriteRequest = Server_pb2.ClientWriteRequest(name = filename, content = content, uuid = file_uuid);
+		clientWriteResponse = server_stub.ClientWrite(clientWriteRequest);
+		print("\nResponse from:",server.address);
+		if clientWriteResponse.status == "SUCCESS":
+			print("STATUS : ", clientWriteResponse.status);
+			print("UUID : ",clientWriteResponse.uuid);
+			print("VERSION : ", clientWriteResponse.timestamp);
+		else:
+			print("STATUS : ", clientWriteResponse.status);
+
+
+
+#-----------------------------------------------------------
+
+
+server_address = "Not connected";
+
+while(True):
+	print("-----------------------------------------");
+	print("Please Enter Number of one of the following options:");
+	print("1. GetServerList");
+	print("2. Write a File");
+	print("3. Read a File");
+	print("4. Delete a File");
+	print("5. Generate UUID ");
+	print("6. EXIT")
+
+	option = input("\n Enter Option Number : ");
+	print("----------------------------------------\n")
+
+	# Case handling - 
+	if option == "1":
+
+		getServerList();
+
+	elif option == "2" :
+
+		filename = input("Enter File Name : ");
+		content = input("Enter Content : ");
+		file_uuid = input("Enter UUID : ");
+		Write(filename, content, file_uuid);
+
+
+	elif option == "3" :
+
+		file_uuid = input("Enter UUID : ");
+		#Read(file_uuid);
+
+
+	elif option == "4" :
+		
+		file_uuid = input("Enter UUID : ");
+		#Delete(file_uuid);
+
+
+	elif option == "5" :
+
+		print("UUID : ",generateUID());
+
+	elif option == "6":
+
+		break;
+
+	else:
+		print("Invalid Input!! Please Enter again.");
--- Quoram-Based/server.py
+++ Quoram-Based/server.py
@@ -8,14 +8,109 @@ from concurrent import futures;
 import RegistryServer_pb2;
 import RegistryServer_pb2_grpc;
 
+# Importing Server Proto - 
+import Server_pb2;
+import Server_pb2_grpc;
+
 # For taking input from commandline
 import sys;
-
-
-
+# For using DATETIME
+from datetime import datetime 
+# For storing information - 
+import os;
 
 # REGISTRY SERVER ADDRESS - 
 REGISTRY_SERVER_ADDRESS = "localhost:8000";
+
+
+
+
+# Service for Writting File
+class ClientWriteServiceServicer(object):
+	def ClientWrite(self, request, context):
+		global FileList ;
+		global server_dir;
+
+		print("RECIEVED FILE WRITE REQUEST FROM CLIENT OF FILE UUID:", request.uuid);
+
+		# Condition checks
+
+		file_with_uuid = None;
+		file_with_filename = None;
+
+		for files in FileList.files:
+			if (files.uuid == request.uuid):
+				file_with_uuid = files;
+				break;
+
+		for files in FileList.files:
+			if (files.filename == request.name):
+				file_with_filename = files;
+				break;
+
+		res_new_file = False;
+		res_update_file = False;
+
+		if file_with_uuid == None:
+			# UUID DOES NOT EXIST 
+			if file_with_filename != None :
+				# UUID DOES NOT EXIST, BUT FILE with Given Name Exist
+				clientWriteResponse = Server_pb2.ClientWriteResponse(status = "FILE WITH SAME NAME ALREADY EXISTS", uuid = None , timestamp = None);
+				return clientWriteResponse;
+			else :
+				# UUID DOES NOT EXIST AND FILE NAME NOT EXIST
+				res_new_file = True;
+		else:
+			# UUID EXISTS
+			if file_with_filename == None:
+				# UUID EXISTS AND FILENAME DOES NOT EXISTS
+				clientWriteResponse = Server_pb2.ClientWriteResponse(status = "DELETED FILE CAN NOT BE UPDATED", uuid = None , timestamp = None);
+				return clientWriteResponse;
+
+			elif (file_with_filename != None) and (file_with_filename != file_with_uuid):
+				# UUID EXISTS, FILENAME EXISTS, But UUID DOES NOT MATCH WITH FILENAME
+				# User is trying to create 2 FILES For same UUID
+				clientWriteResponse = Server_pb2.ClientWriteResponse(status = "FILENAME DOES NOT MATCH WITH UUID", uuid = None , timestamp = None);
+				return clientWriteResponse; 
+			else:
+				# UUID EXISTS, FILENAME EXISTS AND MAPPING EXISTS
+				res_update_file = True;
+
+		if (res_update_file == True) or (res_new_file == True): 
+
+			if (res_update_file == False):
+				new_file = FileList.files.add();
+				new_file.uuid = request.uuid;
+				new_file.filename = request.name;
+
+				curr_now = datetime.now();
+				new_file.timestamp = curr_now.strftime("%d/%m/%Y %H:%M:%S");
+			else:
+				new_file = file_with_uuid;
+				curr_now = datetime.now();
+				new_file.timestamp = curr_now.strftime("%d/%m/%Y %H:%M:%S");
+
+
+			# Writting content : Making directory and saving files
+			filename =  server_dir +"\\"+ str(request.name)+".txt";
+			with open(filename, "w") as file:
+				file.write(request.content);
+
+
+			clientWriteResponse = Server_pb2.ClientWriteResponse(status = "SUCCESS", uuid = new_file.uuid, timestamp = new_file.timestamp);
+			return clientWriteResponse;
+
+		# SOMETHING BAD HAPPENED
+		clientWriteResponse = Server_pb2.ClientWriteResponse(status = "SOMETHING BAD HAPPENED", uuid = None , timestamp = None);
+		return clientWriteResponse;
+
+
+
+
+
+
+# Creating Variable for FileList for storing Filename, uuid and Timestamp
+FileList = Server_pb2.FileList();
 
 if len(sys.argv) != 2:
 	print("Please ENTER address of the server as argument. Usage: python server.py [address]");
@@ -32,3 +127,38 @@ registry_server_request = RegistryServer_pb2.ServerRequest(address =server_addre
 registry_server_response = server_stub.Register(registry_server_request);
 
 print("STATUS:", registry_server_response.status);
+
+
+
+# Creating directories for storing files 
+server_dir = os.path.join(os.getcwd(), "Files");
+
+# If Files Directory not exists
+if not os.path.exists(server_dir):
+	os.mkdir(server_dir);
+
+server_dir = os.path.join(server_dir, server_address[10:]);
+# If Server Directory not exists
+if not os.path.exists(server_dir):
+	os.mkdir(server_dir);
+
+
+
+
+# Creating  Server
+server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+
+# ADDING services
+
+# Adding Write Services - 
+Server_pb2_grpc.add_ClientWriteServiceServicer_to_server(ClientWriteServiceServicer(), server);
+# Adding Read Services -
+#Server_pb2_grpc.add_ClientReadServiceServicer_to_server(ClientReadServiceServicer(), server);
+# Adding Delete Services -
+#Server_pb2_grpc.add_ClientDeleteServiceServicer_to_server(ClientDeleteServiceServicer(), server);
+
+# adding insecure port - 
+server.add_insecure_port(server_address);
+server.start();
+
+server.wait_for_termination();
